49a238d2add1776a845cda222ba7b594e604cd64
yanxhz <adrianduha123@gmail.com>
2023-06-03 22:17:49 +0700

    mengubah tampilan login

--- index.php
+++ index.php
@@ -32,59 +32,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Login Toko Sararaholi Jaya</title>
-
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-</head>
-
-<body class="bg-gradient-primary">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href = "css/login.css">
+  </head>
+  <body>
     <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-            <div class="col-md-5 mt-5">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-
-						<div class="p-5">
-							<div class="text-center">
-								<h4 class="h4 text-gray-900 mb-4"><b>Login Toko Sararaholijaya</b></h4>
-							</div>
-
-							<form method="post">
-								<div class="mb-3">
-									<input type="text" class="form-control form-control-user" name="username"
-										placeholder="User ID" autofocus>
-								</div>
-								<div class="mb-3">
-									<input type="password" class="form-control form-control-user" name="password"
-										placeholder="Password">
-								</div>
-
-									<button class="btn btn-primary" name="login" type="submit">Login</button>
-							</form>
-
-							<hr>
-							<div class="text-center">
-								<a class="small" href="forgot-password.html">Forgot Password?</a>
-							</div>
-						</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div class="login">
+        <form action="" method="post">
+          <h1>Login</h1>
+          <hr />
+          <label for="">Username</label>
+          <input type="text" name="username" placeholder="Username" />
+          <label for="">Password</label>
+          <input type="password" name="password" placeholder="Password" />
+          <button type= "submit" name="login">Login</button>
+          <p>
+            <a href="register.html">Reset Password</a>
+          </p>
+        </form>
+      </div>
+      <div class="right">
+        <img src="img/logo.jpg" alt="" />
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-
-</body>
+  </body>
 </html>
+
