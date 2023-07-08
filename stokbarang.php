c2d8f96a5a01d456fb7c319f9979d3478e6070a4
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2023-07-08 14:51:45 +0700

    Update stokbarang.php

--- stokbarang.php
+++ stokbarang.php
@@ -136,9 +136,6 @@ if (isset($_POST['editstok'])) {
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Stok Barang</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal"data-bs-target="#exampleModal">Tambah Data</button>
-          </div>
         </div>
       </div>
 
