d94164eba3aa40434ba9481679a65c0c5dacf2f9
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2023-07-12 00:08:43 +0700

    update tampilan

--- dashboard.php
+++ dashboard.php
@@ -98,7 +98,7 @@ require 'login.php';
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3">Toko Sararaholi Jaya</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="dashboard.php">Toko Sararaholi Jaya</a>
   
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -118,52 +118,8 @@ require 'login.php';
 </div>
 </header>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="dashboard.php">
-              <span data-feather="home"></span>
-              Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="penjualan.php">
-              <span data-feather="shopping-cart"></span>
-              Penjualan
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="databarang.php">
-              <span data-feather="database"></span>
-              Data Barang
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="stokbarang.php">
-              <span data-feather="layers"></span>
-              Stok Barang
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pembelian.php">
-              <span data-feather="shopping-bag"></span>
-              Pembelian
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="riwayat.php">
-              <span data-feather="archive"></span>
-              Riwayat
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
 
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <main class="col-md-12 ms-sm-auto col-lg-12 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
--- stokbarang.php
+++ stokbarang.php
@@ -70,8 +70,11 @@ if (isset($_POST['editstok'])) {
   <body>
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Toko Sararaholi Jaya</a>
-  
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="dashboard.php">
+      <span data-feather="arrow-left">
+        Kembali
+      </span>
+    </a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -93,12 +96,6 @@ if (isset($_POST['editstok'])) {
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" href="dashboard.php">
-              <span data-feather="home"></span>
-              Dashboard
-            </a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="penjualan.php">
               <span data-feather="shopping-cart"></span>
