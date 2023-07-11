fe109046f36d607f466a4cb36d50b8cb2579a2ef
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2023-07-12 00:11:13 +0700

    update kembali

--- pembelian.php
+++ pembelian.php
@@ -136,8 +136,8 @@ if (isset($_GET['id_pembelian'])) {
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="dashboard.php">
       <span data-feather="arrow-left">
-        Kembali
       </span>
+      Kembali
   </a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
